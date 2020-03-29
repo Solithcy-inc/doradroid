ca84e3d494d0bf987cfbd7250c9f1437231284e2
Solithcy-inc <shards.dust@gmail.com>
2020-03-29 15:49:56 +0100

    Added a sexy database. doracoinsdatabase.py is not included.

--- bot.py
+++ bot.py
@@ -1,1 +1,62 @@
+import discord
+from discord import channel
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions
+import random
+import os
+import asyncio
+import sys
+import json
+import datetime
+import requests
+import time
+import string
+from discord.ext import tasks
+import asyncio
+import logging
+from discord.ext.commands import CommandNotFound
+from discord.ext import commands
+import pymysql
+import doracoinsdatabase as dc
 
+#############
+
+db=dc.connect()
+cursor=db.cursor()
+prefix="dd!"
+bot = commands.AutoShardedBot(command_prefix=prefix)
+bot.remove_command("help")
+TOKEN=open("token.txt", "r").read()
+print(TOKEN)
+
+#############
+
+class CustomCooldown:
+    def __init__(self, rate: int, per: float, alter_rate: int, alter_per: float, bucket: discord.ext.commands.BucketType, *, elements):
+        self.elements = elements
+        self.default_mapping = commands.CooldownMapping.from_cooldown(rate, per, bucket)
+        self.altered_mapping = commands.CooldownMapping.from_cooldown(alter_rate, alter_per, bucket)
+
+    def __call__(self, ctx: commands.Context):
+        skip = False
+        for i in self.elements:
+            if i[0] == ctx.author.id:
+                if int(time.time()) > i[1]:
+                    del i
+                    break
+                else:
+                    skip=True
+                    bucket = self.altered_mapping.get_bucket(ctx.message)
+        if skip == False:
+            bucket = self.default_mapping.get_bucket(ctx.message)
+        retry_after = bucket.update_rate_limit()
+        if retry_after:
+            raise commands.CommandOnCooldown(bucket, retry_after)
+        return True
+
+#############
+
+
+#############
+
+bot.run(TOKEN)
